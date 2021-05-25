d04840ce62d6aca3645bae4a9988b99b8bdaaafd
BoBoBalloon <61363161+BoBoBalloon@users.noreply.github.com>
2021-05-25 18:07:23 -0400

    fixed minor bug where hunter mob was 15-29 instead of 15-30

--- src/main/java/com/runicrealms/plugin/professions/crafting/hunter/HunterPlayer.java
+++ src/main/java/com/runicrealms/plugin/professions/crafting/hunter/HunterPlayer.java
@@ -169,7 +169,7 @@ public class HunterPlayer {
         int index = rand.nextInt(hunterMobs.size());
         TaskMobs mob = hunterMobs.get(index);
 
-        this.setTask(mob, 0, rand.nextInt(15, 30));
+        this.setTask(mob, 0, rand.nextInt(15, 30 + 1)); //+1 because the method is dumb and gens an index
 
         this.player.sendMessage(ColorUtil.format("&r&aYour target is: &r&f" + this.task.getName()));
         this.player.sendMessage(ColorUtil.format("&r&aPlease defeat it &r&f" + this.maxHunterKills + " &r&atimes"));
