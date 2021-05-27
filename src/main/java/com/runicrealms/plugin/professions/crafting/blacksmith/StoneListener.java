edabe7f34981007b7fa9d2069d7e8f5db53e8882
Skyfallin <dimetrius.hightower@gmail.com>
2021-05-26 22:34:36 -0700

    added updated whetstone, sharp stone listener

--- src/main/java/com/runicrealms/plugin/professions/crafting/blacksmith/StoneListener.java
+++ src/main/java/com/runicrealms/plugin/professions/crafting/blacksmith/StoneListener.java
@@ -4,14 +4,12 @@ import com.runicrealms.plugin.RunicProfessions;
 import com.runicrealms.plugin.events.WeaponDamageEvent;
 import com.runicrealms.plugin.item.util.ItemRemover;
 import com.runicrealms.plugin.professions.utilities.itemutil.BlacksmithItems;
-import com.runicrealms.runicitems.RunicItemsAPI;
 import com.runicrealms.runicitems.item.RunicItem;
+import com.runicrealms.runicitems.item.event.RunicItemGenericTriggerEvent;
 import org.bukkit.*;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.HashMap;
@@ -28,26 +26,17 @@ public class StoneListener implements Listener {
     }
 
     @EventHandler
-    public void onInteract(PlayerInteractEvent e) {
-
-        if (e.getItem() == null) return;
-        if (e.getItem().getType() != Material.FLINT) return;
-        if (e.getItem().getItemMeta() == null) return;
-        if (!e.getItem().getItemMeta().getDisplayName().equals(BlacksmithItems.WHETSTONE.getDisplayableItem().getDisplayName())
-                && !e.getItem().getItemMeta().getDisplayName().equals(BlacksmithItems.SHARPENING_STONE.getDisplayableItem().getDisplayName()))
+    public void onRunicClickTrigger(RunicItemGenericTriggerEvent e) {
+        if (!e.getItem().getDisplayableItem().getDisplayName().equals(BlacksmithItems.WHETSTONE.getDisplayableItem().getDisplayName()))
             return;
 
-        Player pl = e.getPlayer();
-        ItemStack stone = e.getItem();
-
-        RunicItem runicItem = RunicItemsAPI.getRunicItemFromItemStack(e.getItem());
+        RunicItem runicItem = e.getItem();
         String bonus = runicItem.getData().get(DATA_DAMAGE_STRING);
         String duration = runicItem.getData().get(DATA_DURATION_STRING);
         int durationInt = Integer.parseInt(duration);
-        Bukkit.broadcastMessage(bonus);
-        Bukkit.broadcastMessage(duration);
+        Player pl = e.getPlayer();
 
-        ItemRemover.takeItem(pl, stone, 1);
+        ItemRemover.takeItem(pl, e.getItemStack(), 1);
         boostedPlayers.put(pl.getUniqueId(), Integer.valueOf(bonus));
         pl.playSound(pl.getLocation(), Sound.BLOCK_ANVIL_PLACE, 0.5f, 2.0f);
         pl.sendMessage
@@ -70,6 +59,7 @@ public class StoneListener implements Listener {
 
     @EventHandler
     public void onWeaponDamage(WeaponDamageEvent e) {
+        if (!e.isAutoAttack()) return;
         if (!boostedPlayers.containsKey(e.getPlayer().getUniqueId())) return;
         double bonus = boostedPlayers.get(e.getPlayer().getUniqueId());
         e.setAmount((int) (e.getAmount() + bonus));
