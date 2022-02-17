f223c83601108704c371a7a756ce0300cad6d09a
skyfallin <dimetrius.hightower@gmail.com>
2022-02-17 13:02:42 -0700

    added description to profession enum

--- src/main/java/com/runicrealms/plugin/professions/Profession.java
+++ src/main/java/com/runicrealms/plugin/professions/Profession.java
@@ -2,18 +2,20 @@ package com.runicrealms.plugin.professions;
 
 public enum Profession {
 
-    ANY("any"),
-    ALCHEMIST("alchemist"),
-    BLACKSMITH("blacksmith"),
-    COOKING("cooking"),
-    ENCHANTER("enchanter"),
-    HUNTER("hunter"),
-    JEWELER("jeweler");
+    ANY("any", ""),
+    ALCHEMIST("alchemist", "Craft powerful potions to aid yourself in battle!"),
+    BLACKSMITH("blacksmith", "Forge armor, weapons, and off-hand weapons!"),
+    COOKING("cooking", ""),
+    ENCHANTER("enchanter", "Enchant scrolls and perform rituals to aid your allies in combat!"),
+    HUNTER("hunter", "Hunt specific monsters to gain hunter shop rewards!"),
+    JEWELER("jeweler", "Cut unique gemstones to increase your combat stats!");
 
     private final String name;
+    private final String description;
 
-    Profession(String name) {
+    Profession(String name, String description) {
         this.name = name;
+        this.description = description;
     }
 
     /**
@@ -23,5 +25,9 @@ public enum Profession {
      */
     public String getName() {
         return this.name.substring(0, 1).toUpperCase() + this.name.substring(1);
+    }
+
+    public String getDescription() {
+        return description;
     }
 }
